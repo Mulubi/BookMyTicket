eba3550ac1da3cfa35851c29341acdcb1bdd2a8b
Mulubi <Mulubiwarren@gmail.com>
2023-02-13 17:56:16 +0300

    Added templates skeleton

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -7,22 +7,22 @@ app.config['SECRET_KEY'] = '41ed75074dc9acfc44d3ca8ab3d6477f'
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Home page html goes here"
+    return render_template("home.html", title="Home-page") 
 
 @app.route('/about')
 def about_page():
-    return "About Page html goes here"
+    return render_template("about.html", title="About-page")
 
 @app.route('/register')
 def register():
     form = RegistrationForm()
-    return "register html goes here"
+    return render_template("register.html", title="Registration Page", form=form)
 
 @app.route('/login')
 def login():
     form = LoginForm()
-    return "login html goes here"
+    return render_template("login.html", title="Login Page", form=form)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
